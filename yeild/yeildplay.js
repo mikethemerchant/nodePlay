763b5b3465353a1fba268a10278a5d5c9b05655b
Michael Bender <michael.bender@spartronics.com>
2023-06-27 14:34:38 -0400

    don't know

---
+++ yeild/yeildplay.js
@@ -0,0 +1,14 @@
+function* foo(index) {
+    while (index < 2) {
+      yield index;
+      index++;
+    }
+  }
+  
+  const iterator = foo(0);
+  
+  console.log(iterator.next().value);
+  // Expected output: 0
+  
+  console.log(iterator.next().value);
+  // Expected output: 1
